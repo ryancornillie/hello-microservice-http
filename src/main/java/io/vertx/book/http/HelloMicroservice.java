a981f3e7e32e3375a4bbec7516584082f2f2800b
cor1ply <Ryan.Cornillie@us.bosch.com>
2018-09-19 14:51:58 -0400

    working routes

--- src/main/java/io/vertx/book/http/HelloMicroservice.java
+++ src/main/java/io/vertx/book/http/HelloMicroservice.java
@@ -1,20 +1,20 @@
 package io.vertx.book.http;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.ext.web.Router;
 
 public class HelloMicroservice extends AbstractVerticle {
 
     @Override
     public void start() {
-
-        Router router = Router.router(vertex);
+        Router router = Router.router(vertx);
         router.get("/").handler(rc -> rc.response().end("hello"));
         router.get("/:name").handler(rc -> rc.response()
-            .end("hello " + rc.pathParam("name")));
-        
+          .end("hello " + rc.pathParam("name")));
+       
         vertx.createHttpServer()
-            .requestHandler(router:accept)
-            .listen(8080);
+          .requestHandler(router::accept)
+          .listen(8080);
     }
 
 }
